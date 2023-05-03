b5efd546758f05898e225dd234581a0f09f686a9
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-05-03 18:27:59 +0200

    initial comic page

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -8,7 +8,37 @@
 {{-- Title --}}
 
 {{-- Main --}}
+
 @section('page.main')
-    <h1>{{ $comic['title'] }}</h1>
+    {{-- Jumbotron --}}
+    <div id="jumbotron"></div>
+    {{-- Jumbotron --}}
+
+    {{-- Comic --}}
+    <div id="comic">
+        <div class="container">
+            <div class="divisor">
+                {{-- Thumbnail --}}
+                <div id="thumb">
+                    <div class="label type">{{ $comic['type'] }}</div>
+                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }} ">
+                    <div class="label gallery">
+                        <a href="#">View Gallery</a>
+                    </div>
+                </div>
+                {{-- Thumbnail --}}
+            </div>
+        </div>
+    
+        {{-- Info --}}
+        <div class="comic-info">
+            <div class="container">
+                <h2>{{ $comic['title'] }}</h2>
+                <div class="description">{{ $comic['description'] }}</div>
+            </div>
+        </div>
+        {{-- Info --}}
+    </div>
+    {{-- Comic --}}
 @endsection
 {{-- Main --}}
